45875b10379d49c65cd70cbe23030699cee906fc
Justin Wagner <jrwagz@gmail.com>
2022-12-19 22:01:56 -0700

    reorganized work folder

---
+++ python_work/ch01/file_extensions.py
@@ -0,0 +1,4 @@
+# Justin Wagner 19-Dec-2022
+# This is a simple example of removing file extensions from string names
+filename = "python_notes.txt"
+print(f'Please refer to {filename.removesuffix(".txt")}')
---
+++ python_work/ch02/number_8.py
@@ -0,0 +1,4 @@
+print(1*8)
+print(3+5)
+print(16/2)
+print(10-2)
